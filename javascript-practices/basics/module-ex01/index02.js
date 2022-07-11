7139fb3450cf80f7becd9e92f305b36f713d9940
Aldebalan <tjdgus4093@naver.com>
2022-07-11 14:31:33 +0900

    require #1

---
+++ javascript-practices/basics/module-ex01/index02.js
@@ -0,0 +1,3 @@
+/* 모듈을 사용하는 app */
+var app = require('./app');
+console.log(App());
---
+++ javascript-practices/basics/module-ex02/index.js
@@ -0,0 +1,5 @@
+var m = require('./math');
+
+console.log(m.PI);
+console.log(m.min(-10, 20, 30, 40));
+console.log(m.max(-10, 20, 30, 40));
