8083fe46879f0e5a1652cbc3a0fb17dc40f23a99
Aldebalan <tjdgus4093@naver.com>
2022-07-12 14:19:48 +0900

    es6 #4

---
+++ javascript-practices/es6/ex09.02.mjs
@@ -0,0 +1,16 @@
+/**
+ * unamed export with default
+ * 
+ * 이름 없는 객체
+ */
+
+export default {
+    add: function(a, b) {
+        return a + b;
+    },
+    substract: function(a, b) {
+        return a - b;
+    }
+}
+
+
