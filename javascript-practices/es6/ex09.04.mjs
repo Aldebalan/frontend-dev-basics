46cf12db1129553ef2b1c04aeb124bd1331c8fd8
Aldebalan <tjdgus4093@naver.com>
2022-07-15 09:49:37 +0900

    updqte

---
+++ javascript-practices/es6/ex09.04.mjs
@@ -0,0 +1,20 @@
+/**
+ * named exportI
+ * 
+ * 1. exports.name = ... : commonJS(require)와 유사하다.
+ * 2. 이 모듈을 import할 때에는 destructing이 가능하다.
+ */
+
+const _a = function(a, b) {
+    return a + b;
+}
+
+const add = function(a, b) {
+    return _a(a, b);
+}
+
+const substract = function(a, b) {
+    return a - b;
+}
+
+export {add, substract}
---
+++ javascript-practices/es6/ex09.05.mjs
@@ -0,0 +1,25 @@
+/**
+ * named & unnamed export
+ * 
+ */
+
+const add = function(a, b) {
+    return a + b;
+}
+
+const substract = function(a, b) {
+    return a - b;
+}
+
+const divide = function(a, b) {
+    return a / b;
+}
+
+const multiply = function(a, b) {
+    return a * b;
+}
+
+export {add, substract}
+export default {divide, multiply}
+
+
