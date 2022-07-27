89d712a9f71b22cf8502bed336763e7ac1d2bb46
Aldebalan <github tjdgus4093@naver.com>
2022-07-27 10:22:25 +0900

    js async

---
+++ javascript-practices/async/ex01.js
@@ -0,0 +1,38 @@
+const fetch = function(params, callback){
+    // 
+    // 비동기 코드(함수): 파일 시스템 접근, 네트워크, SQL to DB, setTimeout
+    //
+    setTimeout(function() {
+        if(params === 'param-data') {
+            callback(null, 'ok');
+        } else {
+            callback(new Error('fail'), null);
+        }
+    }, 2000);
+}
+
+// callback 함수, 데이터는 error, data 표준
+// readFile(options, function(error, data){});
+
+// test01: success
+fetch('param-data', function(error, data){
+    if(error) {
+        console.error(error);
+        return;
+    }
+    
+    console.log(data);
+});
+
+// test01: fail
+fetch('param-error', function(error, data){
+    if(error) {
+        console.error(error);
+        return;
+    }
+    
+    console.log(data);
+});
+
+
+console.log("wait....");
